be31b2161ffc5b27c6cec770b866847dad42861c
annyup <x-annypham@live.com>
2020-05-31 03:53:21 -0400

    updated styles, html and added typewriter

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
+// Typewriter effect
+
+const typed = new Typed('#typed', {
+  stringsElement: '#typed-strings',
+  typeSpeed: 100,
+  backSpeed: 80,
+  smartBackspace: true,
+  loop: true
+});
+
 // Hamburger menu effect
 
 var menu = document.getElementById('nav-Styles');
-    var closeIcon = document.getElementById("toggle");
+var closeIcon = document.getElementById("toggle");
 
-    menu.addEventListener('click', handleMenuClick);
 
-    function handleMenuClick(event) {
-      if (event.target instanceof HTMLAnchorElement) {
-        closeIcon.checked = false;
-      }
-    }
+menu.addEventListener('click', handleMenuClick);
+
+function handleMenuClick(event) {
+  if (event.target instanceof HTMLAnchorElement) {
+      closeIcon.checked = false;
+  }
+}
