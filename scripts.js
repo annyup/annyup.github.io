6226161833076e1263522919dcc4abe8449ecdfb
annyup <x-annypham@live.com>
2020-07-10 00:32:30 -0400

    added skill icons

--- scripts.js
+++ scripts.js
@@ -2,15 +2,17 @@ const portfolio = {};
 
 portfolio.portfolioArray = [
     {
+        title: 'Shopper Mapper',
         url: "assets/portfolio-preview-five.png",
         alt: "Shopper Mapper",
         live: "https://shopper-mapper.github.io/shopper-mapper/",
         github: "https://github.com/shopper-mapper/shopper-mapper",
         description: "Sometimes you don't want to go to the coolest, newest place. Shopper Mapper is the app for you! Made by Anny Pham, Yuliana Hazda, Allan Limitless, and Edward Bacal.",
-        language: "HTML | CSS | ReactJS | Firebase | API | Mob Programming",
+        language: "HTML | CSS | ReactJS | API | Mob Programming",
         aos: 'fade-right',
     },
     {
+        title: 'Pic-draw-sso',
         url: "assets/portfolio-preview-four.png",
         alt: "Pic-draw-sso",
         live: "https://annyup.github.io/pic-draw-sso/",
@@ -20,6 +22,7 @@ portfolio.portfolioArray = [
         aos: 'fade-left',
     },
     {
+        title: 'Emoji Image Gallery',
         url: "assets/portfolio-preview-three.png",
         alt: "Emoji Image Gallery",
         live: "https://anny-hector.github.io/emoji-image-gallery/",
@@ -29,8 +32,9 @@ portfolio.portfolioArray = [
         aos: 'fade-right',
     },
     {
+        title: "I purple you",
         url: "assets/portfolio-preview-two.png",
-        alt: "Mindn'Soul",
+        alt: "I purple you",
         live: "https://annyup.github.io/annyPhamProjectThree/",
         github: "https://github.com/annyup/annyPhamProjectThree",
         description: "I purple you is a timeline and soundboard dedicated to the Kpop group BTS. Interact with the buttons and listen to a short clip of their music!",
@@ -38,6 +42,7 @@ portfolio.portfolioArray = [
         aos: 'fade-left',
     },
     {
+        title: `Mindn'Soul`,
         url: "assets/portfolio-preview-one.png",
         alt: "Mindn'Soul",
         live: "https://annyup.github.io/anny-pham-project-two/",
@@ -56,6 +61,9 @@ portfolio.displayImage = function (portfolioArray) {
         .addClass("portfolio-img")
         .attr("src", image.url)
         .attr("alt", image.alt);
+
+        const title =$(`<p>`)
+            .text(image.title);
 
         const description = $(`<p>`)
             .text(image.description);
@@ -80,7 +88,7 @@ portfolio.displayImage = function (portfolioArray) {
       const overlay = $(`<div>`)
           .addClass("overlay")
           .attr("id", "overlay")
-          .append(description, language, urlButton);
+          .append(title, description, language, urlButton);
       
       const portfolioImg = $(`<div>`)
         .addClass("piece")
@@ -110,7 +118,7 @@ portfolio.hamburgerMenu = function() {
 portfolio.overlayHover = function() {
 
     const overlay = document.getElementById('overlay');
-    overlay.addEventListener('mouseenter', handleOverlay);
+    overlay.addEventListener('click', handleOverlay);
 
     function handleOverlay(event) {
         event.preventDefault();
